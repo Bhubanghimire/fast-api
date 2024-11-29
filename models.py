6973412666e9a8375f0ebd58dc616b8d81069b2e
bhuban <bhuban@nsdai.me>
2024-11-29 13:02:01 +0545

    model update

--- models.py
+++ models.py
@@ -19,4 +19,5 @@ class FilterParams(BaseModel):
     limit: int = Field(100, gt=0, le=100)
     offset: int = Field(0, ge=0)
     order_by: Literal["created_at", "updated_at"] = "created_at"
-    tags: list['str'] = []
+    # tags: list['str'] = []
+
